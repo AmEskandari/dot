13a18a89ed410fa2e6d91f1f17e1b73ff06b4094
ameskandri <amireskandari1998@gmail.com>
2023-03-22 16:14:27 +0330

    needed files are added

---
+++ config.el
@@ -0,0 +1,78 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Amir Eskandari"
+      user-mail-address "am.eskndri@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'tango)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; My selected default directory of emacs file naviagation
+(setq org-directory "/mnt/c/Users/IT CITY/Dropbox/Documents/Org/GTD/Org")
+
+
+(setq org-todo-keywords
+      '((sequence "TODO" "IN PROCESS" "PENDING" "|" "CANCELED" "DONE")))
+
+;; Below command change the default directory
+(setq default-directory "/mnt/c/Users/IT CITY/Dropbox/Documents/Org/GTD/Org" )
+
+
+;; Change state of a task when all subtasks are done,
+;; from https://orgmode.org/manual/Breaking-Down-Tasks.html 
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook #'org-summary-todo)
+
+
+
+(defun org-archive-done-tasks ()
+    "Archive all done tasks."
+    (interactive)
+    (org-map-entries 'org-archive-subtree "/DONE" 'file))
+  (require 'find-lisp)
+  (setq org-agenda-directory (file-truename "/mnt/c/Users/IT CITY/Dropbox/Documents/Org/GTD/Org/Calendars"))
+  (setq org-agenda-files
+        (find-lisp-find-files org-agenda-directory "\.org$"))
+
+
+
+
+
+
+
+
